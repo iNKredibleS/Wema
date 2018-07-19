d74176cec15a3b51fa3bfeddaa9b511577794ac0
Natma Hired <nhired@fb.com>
2018-07-18 17:00:06 -0700

    added RAK features and refresh

---
+++ app/src/main/java/com/fb/inkredibles/myapplication/RAKActivity.java
@@ -0,0 +1,13 @@
+package com.fb.inkredibles.myapplication;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class RAKActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_rak);
+    }
+}
