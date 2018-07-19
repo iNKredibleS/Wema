23cfd91c4790a58d52fd76a31e7b1e2c56b38ba3
Samantha White <samanthawhite@fb.com>
2018-07-19 16:26:41 -0700

    personal log functioning-make query dependent on public or private boolean to do

---
+++ app/src/main/java/com/fb/inkredibles/myapplication/ArchiveActivity.java
@@ -0,0 +1,61 @@
+package com.fb.inkredibles.myapplication;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+
+import com.fb.inkredibles.myapplication.models.Post;
+import com.parse.FindCallback;
+import com.parse.ParseException;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ArchiveActivity extends AppCompatActivity {
+
+    private ArrayList<Post> archivedPosts;
+    private RecyclerView rvArchivePosts;
+    private PostsAdapter archiveAdapter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_archive);
+        //get the recyclerview
+        rvArchivePosts = (RecyclerView) findViewById(R.id.ArchiveRecyclerView);
+        //initialize posts
+        archivedPosts = new ArrayList<>();
+        loadArchives();
+    }
+
+    private void loadArchives(){
+        //get the top posts
+        final Post.Query postsQuery = new Post.Query();
+        postsQuery.getTop().withUser();
+
+        postsQuery.findInBackground(new FindCallback<Post>() {
+            @Override
+            public void done(List<Post> objects, ParseException e) {
+                if (e == null){
+                    for (int i = 0; i < objects.size(); i++){
+                        archivedPosts.add(objects.get(i));
+                        Log.d("Post", archivedPosts.get(i).getMessage());
+                    }
+                    //create a posts adapter
+                    archiveAdapter = new PostsAdapter(archivedPosts);
+                    //attach the adapter to the recyclerview to populate items
+                    rvArchivePosts.setAdapter(archiveAdapter);
+
+                    LinearLayoutManager mLayoutManager = new LinearLayoutManager(ArchiveActivity.this);
+                    mLayoutManager.setReverseLayout(true);
+                    mLayoutManager.setStackFromEnd(true);
+                    rvArchivePosts.setLayoutManager(mLayoutManager);
+                }else{
+                    e.printStackTrace();
+                }
+            }
+        });
+    }
+}
--- app/src/main/java/com/fb/inkredibles/myapplication/MainActivity.java
+++ app/src/main/java/com/fb/inkredibles/myapplication/MainActivity.java
@@ -1,21 +1,9 @@
 package com.fb.inkredibles.myapplication;
 
 import android.content.Intent;
-
-import android.support.v7.app.AppCompatActivity;
-
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
-import android.widget.Button;
-
-import butterknife.BindView;
-import butterknife.ButterKnife;
-
-import android.view.View;
-import android.widget.Button;
-
-
 import android.widget.Button;
 
 import butterknife.BindView;
@@ -27,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
 
 
     Button rakBtn;
+    public static boolean archive;
 
     //TODO be sure to remove this button and intent
     @BindView(R.id.btnCreate) Button button;
@@ -49,16 +38,6 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
-
-
-    }
-
-    @OnClick(R.id.btnCreate)
-    protected void launchCreate(){
-        startActivity(new Intent(MainActivity.this, CreatePostActivity.class));
-
-
-        ButterKnife.bind(this);
         btGrid.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
@@ -67,7 +46,26 @@ public class MainActivity extends AppCompatActivity {
             }
         });
 
+
+
     }
+
+    @OnClick(R.id.btnCreate)
+    protected void launchCreate(){
+        startActivity(new Intent(MainActivity.this, CreatePostActivity.class));
+    }
+
+    @OnClick(R.id.btn_archive)
+    protected void launchArchive(){
+        archive = true;
+        startActivity(new Intent (MainActivity.this, ArchiveActivity.class));
+    }
+
+    public boolean getArchive(){
+        return archive;
+    }
+
+
 
 
 }
